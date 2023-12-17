aa647efe6e6748e31e260e88b6fc0e411a51ccce
Mikhail <109112napoleon@gmail.com>
2023-12-17 22:46:52 +0300

    Commit №3

--- main.py
+++ main.py
@@ -45,3 +45,17 @@ print(names[product])
 for product in products:
   print(max(names[product], key=len))
   print(min(names[product], key=len))
+
+
+#Задача №1
+import random
+import string
+
+products = ['Одежда', 'Телефон', 'Напиток', 'Видеоигра', 'Овощ', 'Обувь', 'Книга', 'Машина', 'Мебель', 'Квартира']
+
+codes = []
+
+for product in products:
+    codes.append(product+str(random.randrange(1, 100)))
+
+print(codes)
